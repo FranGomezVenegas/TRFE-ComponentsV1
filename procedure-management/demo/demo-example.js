3e850dda3d9f326430f647d867f85329ea7e9850
mahdiridho <mahdi.ridho@gmail.com>
2021-10-12 23:52:31 +1000

    Start procedure management component

---
+++ procedure-management/demo/demo-example.js
@@ -0,0 +1,57 @@
+import { LitElement, html, css } from 'lit-element';
+import '@trazit/platform-login/platform-login';
+import '../procedure-management';
+
+class DemoExample extends LitElement {
+  static get styles() {
+    return css`
+        div[hidden] {
+          display: none;
+        }
+      `
+  }
+
+  static get properties() {
+    return {
+      auth: { type: Boolean }
+    }
+  }
+
+  constructor() {
+    super();
+    this.auth = false;
+  }
+
+  render() {
+    return html`
+      <platform-login @authorized=${e=>this.auth=e.target.auth}></platform-login>
+      <div ?hidden="${!this.auth}">
+        <h1>Hi ${this.getUser()}, you are authorized</h1>
+        <procedure-management></procedure-management><br>
+        <button @click=${()=>this.pLogin.logout()}>Logout</button>
+      </div>
+    `;
+  }
+
+  get pLogin() {
+    return this.shadowRoot.querySelector("platform-login")
+  }
+
+  /**
+   * Lifecycle called after DOM updated on the first time
+   * Pulling the app config and waiting for the sts state
+   */
+  firstUpdated() {
+    fetch("./config.json").then(r => r.json()).then(j => {
+      this.pLogin.config = j
+    })
+  }
+
+  getUser() {
+    if (this.auth) {
+      let session = this.pLogin.getUser()
+      return session.header_info.first_name +" "+ session.header_info.last_name +"("+ session.userRole +")"
+    }
+  }
+}
+customElements.define('demo-example', DemoExample);
---
+++ procedure-management/src/ProcedureManagement.js
@@ -0,0 +1,25 @@
+import { html, css, LitElement } from 'lit';
+
+export class ProcedureManagement extends LitElement {
+  static get styles() {
+    return css``;
+  }
+
+  static get properties() {
+    return {
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  firstUpdated() {
+  }
+
+  render() {
+    return html`
+      <h2>Procedure Management</h2>
+    `;
+  }
+}
