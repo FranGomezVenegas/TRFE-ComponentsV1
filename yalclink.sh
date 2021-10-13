8e8a18b71355ddcadb1085408fd287403c68f55c
mahdiridho <mahdi.ridho@gmail.com>
2021-10-13 21:58:51 +1000

    Publish user profile to local yalc

--- yalclink.sh
+++ yalclink.sh
@@ -6,7 +6,7 @@ yalcDir=`yalc dir`
 rm -rf $yalcDir/packages/@trazit
 
 find . \( -name 'node_modules' \) -or \( -name 'package-lock.json' \) -or \( -name 'yalc.lock' \) -or \( -name '.yalc' \) | xargs rm -rf
-requirements="platform-login procedure-management"
+requirements="platform-login procedure-management user-profile"
 for r in $requirements; do
   pushd $r;
   ./install.sh $0;
