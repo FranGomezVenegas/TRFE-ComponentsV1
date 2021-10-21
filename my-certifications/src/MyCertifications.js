d1a253ca8dd723f6e661eac4503de16787708c53
mahdiridho <mahdi.ridho@gmail.com>
2021-10-22 00:01:48 +1000

    my-certifications core component done

--- my-certifications/src/MyCertifications.js
+++ my-certifications/src/MyCertifications.js
@@ -2,6 +2,7 @@ import { html, css } from 'lit';
 import { CommonCore } from '@trazit/common-core';
 import { Layouts } from '@collaborne/lit-flexbox-literals';
 import '@material/mwc-icon-button';
+import './certification-item';
 
 export class MyCertifications extends CommonCore {
   static get styles() {
@@ -36,6 +37,11 @@ export class MyCertifications extends CommonCore {
 
   render() {
     return html`
+      <div class="layout horizontal flex wrap">
+      ${this.certSet.map(c=>
+        html`<certification-item .cert=${c} @mark-complete=${this.markComplete}></certification-item>`
+      )}
+      </div>
     `;
   }
 
@@ -47,9 +53,9 @@ export class MyCertifications extends CommonCore {
 
   pass(type) {
     if (type == "sop") {
-      this.certSet = this.sops.filter(s => s.status == "PASS")
+      this.certSet = this.sops
     } else {
-      this.certSet = this.analytics.filter(s => s.status == "PASS")
+      this.certSet = this.analytics
     }
     this.requestUpdate()
   }
@@ -61,5 +67,59 @@ export class MyCertifications extends CommonCore {
       this.certSet = this.analytics.filter(s => s.status == "NOT_PASS")
     }
     this.requestUpdate()
+  }
+
+  markComplete(e) {
+    // non sop
+    if (!e.detail.user_sop_id) return
+
+    this.fetchApi(this.config.backendUrl + this.config.ApiSopUserUrl + '?' + new URLSearchParams({
+      dbName: this.config.dbName,
+      actionName: 'SOP_MARK_AS_COMPLETED',
+      procInstanceName: e.detail.procedure_name,
+      sopName: e.detail.sop_name,
+      finalToken: JSON.parse(sessionStorage.getItem("userSession")).finalToken      
+    })).then(j => {
+      this.frontEndSopUserAPI(e.detail)
+    })
+  }
+
+  frontEndSopUserAPI(cert) {
+    this.fetchApi(this.config.backendUrl + this.config.frontEndSopUrl + '?' + new URLSearchParams({
+      dbName: this.config.dbName,
+      actionName: 'ALL_IN_ONE',
+      finalToken: JSON.parse(sessionStorage.getItem("userSession")).finalToken      
+    })).then(j => {
+      this.getProcedureList(cert)
+    })
+  }
+
+  getProcedureList(cert) {
+    this.fetchApi(this.config.backendUrl + this.config.frontEndSopUrl + '?' + new URLSearchParams({
+      dbName: this.config.dbName,
+      finalToken: JSON.parse(sessionStorage.getItem("userSession")).finalToken      
+    })).then(j => {
+      let certs = JSON.stringify(this.certSet) // temp ref
+      certs = JSON.parse(certs)
+      this.certSet = []
+      // updating userSession data
+      let userSession = JSON.parse(sessionStorage.getItem("userSession"))
+      if (cert.user_sop_id) { // sop
+        this.sops.forEach((c,i) => {
+          if (c.user_sop_id == cert.user_sop_id) {
+            this.sops[i].status = "PASS"
+          }
+        })
+        userSession.all_my_sops[0].my_sops = this.sops
+      }
+      certs.forEach((c,i) => {
+        if (c.user_sop_id == cert.user_sop_id) {
+          certs[i].status = "PASS"
+        }
+      })
+      sessionStorage.setItem('userSession', JSON.stringify(userSession))
+      this.certSet = certs
+      this.requestUpdate()
+    })
   }
 }
---
+++ my-certifications/src/certification-item.js
@@ -0,0 +1,65 @@
+import { LitElement, html, css } from 'lit';
+import { Layouts } from '@collaborne/lit-flexbox-literals';
+import '@material/mwc-icon';
+import '@material/mwc-icon-button';
+
+export class CertificationItem extends LitElement {
+  static get styles() {
+    return [
+      Layouts,
+      css`
+      :host {
+        display: block;
+        margin: 10px;
+      }
+      :host([hidden]) {
+        display: none;
+      }
+      .card {
+        background-color:  rgba(177, 242, 244); 
+        border-radius: 5px;
+        box-shadow: 1px 1px #888888;
+        padding: 20px;
+        width: 300px;
+        height: 300px;
+        line-height: 1.5;
+      }
+      @media (max-width: 460px) {
+      }
+    `];
+  }
+
+  static get properties() {
+    return {
+      cert: { type: Object },
+      divHeight: { type: String }
+    };
+  }
+
+  constructor() {
+    super();
+    this.cert = {};
+    this.divHeight = "";
+  }
+
+  render() {
+    return html`
+      <div class="card layout vertical center">
+        <mwc-icon-button icon="picture_as_pdf" @click=${()=>window.open(this.cert.file_link, '_blank').focus()}></mwc-icon-button>
+        <div><b>Procedure: </b>${this.cert.procedure_name}</div>
+        <div><b>Name: </b>${this.cert.sop_name}</div>
+        <div><b>Summary: </b>${this.cert.brief_summary}</div>
+        <div><b>My Certification Status: </b>${this.cert.status}</div>
+        <div><mwc-icon icon="${this.cert.status=="PASS"?'bookmark':'warning'}"></mwc-icon></div>
+        <div>${this.cert.status=="NOT_PASS" ? 
+          html`<mwc-icon-button icon="replay" @click=${()=>this.dispatchEvent(new CustomEvent('mark-complete', {
+            detail: this.cert,
+            bubbles: true,
+            composed: true
+          }))}></mwc-icon-button>` : null 
+        }</div>
+      </div>
+    `;
+  }
+}
+window.customElements.define('certification-item', CertificationItem);
