63e6afa11224f220b59ee58bbeb87a17224d7146
mahdiridho <mahdi.ridho@gmail.com>
2021-11-01 23:01:06 +1000

    Publish sample-pending

--- yalclink.sh
+++ yalclink.sh
@@ -6,7 +6,7 @@ yalcDir=`yalc dir`
 rm -rf $yalcDir/packages/@trazit
 
 find . \( -name 'node_modules' \) -or \( -name 'package-lock.json' \) -or \( -name 'yalc.lock' \) -or \( -name '.yalc' \) | xargs rm -rf
-requirements="common-core platform-login procedure-management my-incidents my-certifications user-profile video-tutorial platform-notif"
+requirements="common-core platform-login procedure-management my-incidents my-certifications user-profile video-tutorial platform-notif sample-pending"
 for r in $requirements; do
   pushd $r;
   ./install.sh $0;
