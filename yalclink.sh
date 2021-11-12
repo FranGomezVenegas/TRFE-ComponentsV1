c0e62615e2aeb4de76a66bc35edd8fed493ea766
mahdiridho <mahdi.ridho@gmail.com>
2021-11-12 23:45:52 +1000

    Publish plate reading to yalc

--- yalclink.sh
+++ yalclink.sh
@@ -6,7 +6,7 @@ yalcDir=`yalc dir`
 rm -rf $yalcDir/packages/@trazit
 
 find . \( -name 'node_modules' \) -or \( -name 'package-lock.json' \) -or \( -name 'yalc.lock' \) -or \( -name '.yalc' \) | xargs rm -rf
-requirements="common-core platform-login procedure-management my-incidents my-certifications user-profile video-tutorial platform-notif procedures-core samples-sampling"
+requirements="common-core platform-login procedure-management my-incidents my-certifications user-profile video-tutorial platform-notif procedures-core samples-sampling plate-reading"
 for r in $requirements; do
   pushd $r;
   ./install.sh $0;
