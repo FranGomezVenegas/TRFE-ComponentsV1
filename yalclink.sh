c100efbe68040545288229991c3fe72777847661
mahdiridho <mahdi.ridho@gmail.com>
2021-11-19 22:36:30 +1000

    Yalc publish relogin-dialog

--- yalclink.sh
+++ yalclink.sh
@@ -6,7 +6,7 @@ yalcDir=`yalc dir`
 rm -rf $yalcDir/packages/@trazit
 
 find . \( -name 'node_modules' \) -or \( -name 'package-lock.json' \) -or \( -name 'yalc.lock' \) -or \( -name '.yalc' \) | xargs rm -rf
-requirements="common-core tr-dialog platform-login procedure-management my-incidents my-certifications user-profile video-tutorial platform-notif procedures-core samples-sampling plate-reading"
+requirements="common-core tr-dialog platform-login relogin-dialog procedure-management my-incidents my-certifications user-profile video-tutorial platform-notif procedures-core samples-sampling plate-reading"
 for r in $requirements; do
   pushd $r;
   ./install.sh $0;
