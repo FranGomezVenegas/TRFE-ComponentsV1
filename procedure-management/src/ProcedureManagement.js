fa1b3f436009e3ab3c1cc9015f8672146c85ba54
mahdiridho <mahdi.ridho@gmail.com>
2022-02-15 08:49:41 +1000

    ProcedureManagement fixed endpoint

--- procedure-management/src/ProcedureManagement.js
+++ procedure-management/src/ProcedureManagement.js
@@ -1,4 +1,4 @@
-import { html } from 'lit';
+import { html, nothing } from 'lit';
 import { CommonCore } from '@trazit/common-core';
 import '@alenaksu/json-viewer';
 
@@ -27,19 +27,13 @@ export class ProcedureManagement extends CommonCore {
 
   async authorized() {
     super.authorized()
-    this.defs = []
-    let procedures = JSON.parse(sessionStorage.getItem("userSession")).procedures_list.procedures
-    for (let i=0; i<procedures.length; i++) {
-      await this.fetchApi(this.config.backendUrl + this.config.procDefinitionApiUrl + '?' + new URLSearchParams({
-        actionName: "ALL_PROCEDURE_DEFINITION",
-        procInstanceName: procedures[i].procInstanceName,
-        dbName: this.config.dbName,
-        finalToken: JSON.parse(sessionStorage.getItem("userSession")).finalToken
-      }), false, false).then(j => {
-        this.defs.push(j.definition)
-      })
-    }
-    console.log(this.defs)
+    await this.fetchApi(this.config.backendUrl + this.config.procDefinitionApiUrl + '?' + new URLSearchParams({
+      actionName: "ALL_PROCEDURES_DEFINITION",
+      dbName: this.config.dbName,
+      finalToken: JSON.parse(sessionStorage.getItem("userSession")).finalToken
+    }), false, false).then(j => {
+      this.defs = j.procedures_list.procedures
+    })
     this.requestUpdate()
   }
 }
