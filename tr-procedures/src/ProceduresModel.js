b790bd6ba3a1eb39a47c3c900bd4f371de0346ec
mahdiridho <mahdi.ridho@gmail.com>
2022-03-23 01:29:42 +1000

    em-air local model

--- tr-procedures/src/ProceduresModel.js
+++ tr-procedures/src/ProceduresModel.js
@@ -1,11 +1,13 @@
 import { EmDemoA } from './em-demo-a';
 import { ProcDeploy } from './proc-deploy';
 import { AppProc } from './app-proc';
+import { EmAir } from './em-air';
 import { App } from './app';
 
 export const ProceduresModel = {
   ['em-demo-a']: EmDemoA,
   ['proc-deploy']: ProcDeploy,
   ['app-proc']: AppProc,
+  ['em-air-spr1']: EmAir,
   ['app']: App
 }
