7d2afa058915e501618acb56ce05aa99e9764d2a
frangomezvenegas <info.fran.gomez@gmail.com>
2022-04-03 15:20:00 +0200

    added genoma-1 proc model json

--- tr-procedures/src/ProceduresModel.js
+++ tr-procedures/src/ProceduresModel.js
@@ -3,11 +3,13 @@ import { ProcDeploy } from './proc-deploy';
 import { AppProc } from './app-proc';
 import { EmAir } from './em-air';
 import { App } from './app';
+import { Genoma1 } from './genoma-1';
 
 export const ProceduresModel = {
   ['em-demo-a']: EmDemoA,
   ['proc-deploy']: ProcDeploy,
   ['app-proc']: AppProc,
   ['em-air-spr1']: EmAir,
-  ['app']: App
+  ['app']: App,
+  ['genoma-1']: Genoma1
 }
