56c74dd77d2cd167b760cc9af53c12a9f627a3ae
frangomezvenegas <info.fran.gomez@gmail.com>
2022-09-25 15:23:48 +0200

    fixes for this component

--- holiday-calendars/install.sh
+++ holiday-calendars/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-../install.sh @trazit/common-core @trazit/tr-dialog @trazit/platform-login @trazit/relogin-dialog @trazit/cred-dialog
+../install.sh @trazit/common-core @trazit/tr-dialog @trazit/platform-login @trazit/relogin-dialog @trazit/cred-dialog @trazit/tr-procedures
--- holiday-calendars/src/CalendarDialogTemplate.js
+++ holiday-calendars/src/CalendarDialogTemplate.js
@@ -16,17 +16,60 @@ export function CalendarDialogTemplate(base) {
         // deactivatedObjects: { type: Array },
         // dataForDialog: { type: Object },
         // fromGrid: { type: Boolean }
+        selectedAction: {type: Array}
       }
     }
 
     constructor() {
       super()
+      this.selectedAction = []
     //   this.numDays = 7
     //   this.deactivatedObjects = []
     //   this.fromGrid = false
     }
 
 
+
+    defaultValue(){
+        //console.log('defaultValue')
+        this.resetFields()
+        let dlgFlds=this.actionBeingPerformedModel.dialogInfo.fields
+        if (dlgFlds===undefined){
+            //alert('The dialog '+this.actionBeingPerformedModel.dialogInfo.name+' has no fields property for adding the fields, please review.')
+            return
+        }
+        for (let i=0;i<dlgFlds.length;i++){
+            let fldObj=dlgFlds[i]
+            let keyName=Object.keys(fldObj)
+            
+            //if (==null){            
+            if (fldObj[keyName].default_value!==undefined&&fldObj[keyName].default_value!==null){
+                this[keyName[0]].value=fldObj[keyName].default_value
+            }
+            if (fldObj[keyName].selObjectPropertyName!==undefined&&fldObj[keyName].selObjectPropertyName!==null){
+                this[keyName[0]].value=this.selectedItems[0][fldObj[keyName].selObjectPropertyName]
+            }
+            if (fldObj[keyName].internalVariableObjName!==undefined&&fldObj[keyName].internalVariableObjName!==null&&
+                fldObj[keyName].internalVariableObjProperty!==undefined&&fldObj[keyName].internalVariableObjProperty!==null){
+                this[keyName[0]].value=this[fldObj[keyName].internalVariableObjName][0][fldObj[keyName].internalVariableObjProperty]
+            }
+        }
+    }    
+    resetFields(){           
+        //alert('reset Fields now')   
+        let dlgFlds=this.actionBeingPerformedModel.dialogInfo.fields
+        if (dlgFlds===undefined){
+            //alert('The dialog '+this.actionBeingPerformedModel.dialogInfo.name+' has no fields property for adding the fields, please review.')
+            return
+        }
+        for (let i=0;i<dlgFlds.length;i++){
+            let fldObj=dlgFlds[i]
+            let keyName=Object.keys(fldObj)
+            if (this[keyName]!==null){
+                this[keyName[0]].value=""
+            }
+        }
+    }
     cleanFormFields(){
         if (this.text1){this.text1.value=''}
         if (this.number1){this.number1.value=''}
@@ -45,46 +88,46 @@ export function CalendarDialogTemplate(base) {
         @closed=${() => this.cleanFormFields()}
         scrimClickAction="">
             ${this.selectedAction===undefined||this.selectedAction.dialogInfo===undefined ?
-            html``: html`    
+            html`hola`: html`    
             <div class="layout vertical flex center-justified">
-            ${!this.selectedAction.dialogInfo.fieldText.text1 ?
+            ${!this.selectedAction.dialogInfo.fields.text1 ?
                 html``: html`        
                 <div class="layout horizontal flex center-center">
                 <mwc-textfield class="layout flex" id="text1" type="text" 
-                .value=${this.selectedAction.dialogInfo.fieldText.text1.default_value ? this.selectedAction.dialogInfo.fieldText.text1.default_value : ''} @change=${e => this.numDays = e.target.value}
-                label="${this.selectedAction.dialogInfo.fieldText.text1["label_" + this.lang]}"
+                .value=${this.selectedAction.dialogInfo.fields.text1.default_value ? this.selectedAction.dialogInfo.fields.text1.default_value : ''} @change=${e => this.numDays = e.target.value}
+                label="${this.selectedAction.dialogInfo.fields.text1["label_" + this.lang]}"
                 @keypress=${e => e.keyCode == 13 && this.genomaSuperDialogClickedActionNoCredChecker()}></mwc-textfield>
                 </div>
             `}          
-            ${!this.selectedAction.dialogInfo.fieldText.text2 ?
+            ${!this.selectedAction.dialogInfo.fields.text2 ?
               html``: html`        
               <div class="layout horizontal flex center-center">
               <mwc-textfield class="layout flex" id="text2" type="text" 
-              .value=${this.selectedAction.dialogInfo.fieldText.text2.default_value ? this.selectedAction.dialogInfo.fieldText.text2.default_value : ''} @change=${e => this.numDays = e.target.value}
-              label="${this.selectedAction.dialogInfo.fieldText.text2["label_" + this.lang]}"
+              .value=${this.selectedAction.dialogInfo.fields.text2.default_value ? this.selectedAction.dialogInfo.fields.text2.default_value : ''} @change=${e => this.numDays = e.target.value}
+              label="${this.selectedAction.dialogInfo.fields.text2["label_" + this.lang]}"
               @keypress=${e => e.keyCode == 13 && this.genomaSuperDialogClickedActionNoCredChecker()}></mwc-textfield>
               </div>
             `}          
-            ${!this.selectedAction.dialogInfo.fieldText.number1 ?
+            ${!this.selectedAction.dialogInfo.fields.number1 ?
                 html``: html`        
                 <div class="layout horizontal flex center-center">
                 <mwc-textfield class="layout flex" id="number1" type="number" 
-                .value=${this.selectedAction.dialogInfo.fieldText.number1.default_value ? this.selectedAction.dialogInfo.fieldText.number1.default_value : ''} @change=${e => this.numDays = e.target.value}
-                label="${this.selectedAction.dialogInfo.fieldText.number1["label_" + this.lang]}"
+                .value=${this.selectedAction.dialogInfo.fields.number1.default_value ? this.selectedAction.dialogInfo.fields.number1.default_value : ''} @change=${e => this.numDays = e.target.value}
+                label="${this.selectedAction.dialogInfo.fields.number1["label_" + this.lang]}"
                 @keypress=${e => e.keyCode == 13 && this.genomaSuperDialogClickedActionNoCredChecker()}></mwc-textfield>
                 </div>
             `}          
-            ${!this.selectedAction.dialogInfo.fieldText.list1 ?
+            ${!this.selectedAction.dialogInfo.fields.list1 ?
             html``: html`        
-                <mwc-select id="list1" label="${this.selectedAction&&this.selectedAction.dialogInfo&&this.selectedAction.dialogInfo.fieldText.list1&&this.selectedAction.dialogInfo.fieldText.list1["label_" + this.lang]}">
-                ${this.selectedAction.dialogInfo.fieldText.list1.items.map((c, i) =>
+                <mwc-select id="list1" label="${this.selectedAction&&this.selectedAction.dialogInfo&&this.selectedAction.dialogInfo.fields.list1&&this.selectedAction.dialogInfo.fields.list1["label_" + this.lang]}">
+                ${this.selectedAction.dialogInfo.fields.list1.items.map((c, i) =>
                     html`<mwc-list-item value="${c.keyName}" ?selected=${i == 0}>${c["keyValue_" + this.lang]}</mwc-list-item>`
                 )}
                 </mwc-select>
             `}    
-            ${this.selectedAction.dialogInfo.fieldText.date1 ?
+            ${this.selectedAction.dialogInfo.fields.date1 ?
               html`
-              <label for="fname">${this.selectedAction.dialogInfo.fieldText.date1["label_" + this.lang]}</label><br>
+              <label for="fname">${this.selectedAction.dialogInfo.fields.date1["label_" + this.lang]}</label><br>
               <input id="date1" type="date" dialogInitialFocus>
               ` : nothing
             }
