d74b88c9d308c05d72831cfe4621dfe2455b91e6
frangomezvenegas <info.fran.gomez@gmail.com>
2022-11-26 00:13:37 +0100

    fixes in tabs component

--- tr-procedures/src/components/Tabs/TabFunctions.js
+++ tr-procedures/src/components/Tabs/TabFunctions.js
@@ -1,9 +1,10 @@
-import { html, css } from 'lit';
+import { html, css , nothing} from 'lit';
 import { columnBodyRenderer } from 'lit-vaadin-helpers';
 
 export function TabFunctions(base) {
     return class extends base {
-        showTabElement(tabBtns){
+        showTabElement(viewModelFromProcModel, hardcodedTbs={}){
+          console.log('showTabElement', 'viewModelFromProcModel', viewModelFromProcModel, 'hardcodedTbs', hardcodedTbs)
             return html`
             <div class="layout flex">
             <div class="layout horizontal center">
@@ -11,9 +12,24 @@ export function TabFunctions(base) {
               </mwc-icon-button>
               <div class="tabContainer layout horizontal flex center">
                 <mwc-icon-button icon="refresh" @click=${this.resetView} ?disabled=${this.samplesReload}></mwc-icon-button>
-                ${(tabBtns[this.procName]||tabBtns.default).map(t =>
-                  html`<tab-element .lang=${this.lang} .tab=${t} @tab-rendered=${this.isScroll} @tab-change=${this.tabChanged} ?disabled=${this.samplesReload}></tab-element>`
+                ${viewModelFromProcModel!==undefined&&viewModelFromProcModel.tabs!==undefined  ?
+                html`
+                  ${(viewModelFromProcModel.tabs).map(t =>                    
+                      html`
+                      ${t.display===undefined||t.display===true  ?
+                        html`<tab-element .lang=${this.lang} .tab=${t} @tab-rendered=${this.isScroll} @tab-change=${this.tabChanged} ?disabled=${this.samplesReload}></tab-element>`
+                      :nothing}
+                    `  
+                  )}
+                `:html`
+                ${(hardcodedTbs.tabs).map(t =>
+                  html`
+                  ${t.display===undefined||t.display===true  ?
+                    html`<tab-element .lang=${this.lang} .tab=${t} @tab-rendered=${this.isScroll} @tab-change=${this.tabChanged} ?disabled=${this.samplesReload}></tab-element>`
+                  :nothing}
+                  `                      
                 )}
+              `}
               </div>
               <mwc-icon-button class="slide" icon="navigate_next" @click=${this.nextTab} ?hidden=${!this.next}>
               </mwc-icon-button>
