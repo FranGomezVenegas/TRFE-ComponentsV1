fe3d90af2ed375ae99b12b610b6e135592d146c3
frangomezvenegas <info.fran.gomez@gmail.com>
2023-05-30 15:07:09 +0200

    added refresh for master data for any action that contains master_data in its response

--- common-core/src/CommonCore.js
+++ common-core/src/CommonCore.js
@@ -173,6 +173,126 @@ export class CommonCore extends LitElement {
         throw err
       }
     }).then(j => {
+      this.refreshMasterData(j)
+      if (feedback) {
+        this.dispatchEvent(new CustomEvent('success', {
+          detail: {...j, log: log},
+          bubbles: true,
+          composed: true
+        }))
+      }
+      return j
+    }).catch(e => {
+      if (e.message == "Unexpected end of JSON input") {
+        this.dispatchEvent(new CustomEvent("error", {
+          detail: {...e},
+          bubbles: true,
+          composed: true
+        }))
+        return
+      } else {
+        this.dispatchEvent(new CustomEvent("error", {
+          detail: {...e, log: log},
+          bubbles: true,
+          composed: true
+        }))
+        this.error(e)
+        return e
+      }
+    })
+  }
+  refreshMasterData(endPointResponse) {
+    console.log('refreshMasterDataaaa', 'procInstanceName', this.procInstanceName, 'endPointResponse', endPointResponse)
+    let userSession = JSON.parse(sessionStorage.getItem("userSession"))
+    if (endPointResponse===undefined||endPointResponse.master_data===undefined) {
+      return
+    } 
+    userSession.procedures_list.procedures[this.procInstanceName].master_data=endPointResponse.master_data
+      // userSession.all_my_sops = j.all_my_sops
+      // userSession.my_pending_sops = j.my_pending_sops
+      // // adjust the new_definition only
+      // j.procedures_list.procedures.forEach(pl => {
+      //   let uid = userSession.procedures_list.procedures.findIndex(l => l.name == pl.name)
+      //   userSession.procedures_list.procedures[uid].new_definition = pl.new_definition
+      //   userSession.procedures_sops = j.procedures_sops
+      //   userSession.sop_tree_list_element = j.sop_tree_list_element
+      // })        
+    sessionStorage.setItem('userSession', JSON.stringify(userSession))
+    return
+    let apiUrl = cert.method_name ? this.config.frontEndAnalysisUrl : this.config.frontEndSopUrl
+    this.fetchApi(this.config.backendUrl + apiUrl + '?' + new URLSearchParams({
+      dbName: this.config.dbName,
+      actionName: cert.method_name ? 'ALL_MY_ANA_METHOD_CERTIF' : 'ALL_IN_ONE',
+      finalToken: JSON.parse(sessionStorage.getItem("userSession")).finalToken      
+    }), false).then(j => {
+      if (j && !j.is_error) {
+        // updating userSession data
+        let userSession = JSON.parse(sessionStorage.getItem("userSession"))
+        if (cert.method_name) {
+          userSession.all_my_analysis_methods = j
+        } else {
+          userSession.all_my_sops = j.all_my_sops
+          userSession.my_pending_sops = j.my_pending_sops
+          // adjust the new_definition only
+          j.procedures_list.procedures.forEach(pl => {
+            let uid = userSession.procedures_list.procedures.findIndex(l => l.name == pl.name)
+            userSession.procedures_list.procedures[uid].new_definition = pl.new_definition
+            userSession.procedures_sops = j.procedures_sops
+            userSession.sop_tree_list_element = j.sop_tree_list_element
+          })
+        }
+        sessionStorage.setItem('userSession', JSON.stringify(userSession))
+        this.dispatchEvent(new CustomEvent('certs-updated'))
+        this.sops = userSession.all_my_sops.length ? userSession.all_my_sops[0].my_sops : this.sops
+        this.analytics = userSession.all_my_analysis_methods.length ? userSession.all_my_analysis_methods[0].my_analysis_method_certifications : this.analytics        
+        this.myPendingCertifApprovals = userSession.all_my_pending_certif_approvals.num_objects>0 ? userSession.all_my_pending_certif_approvals.objects : this.myPendingCertifApprovals
+        let certs = JSON.stringify(this.certSet) // temp ref
+        certs = JSON.parse(certs)
+        certs.forEach((c,i) => {
+          if (cert.method_name) {
+            if (c.id == cert.id) {
+              certs[i].status = "PASS"
+            }
+          } else {
+            if (c.user_sop_id == cert.user_sop_id) {
+              certs[i].status = "PASS"
+            }  
+          }
+        })
+        this.certSet = certs
+        this.requestUpdate()
+      }
+    })
+  }  
+  /**
+   * Populating fetch api by post
+   * @param {*} urlParams the url api with params
+   * @param {*} feedback will be show up the user feedback
+   */
+  fetchApiPost(urlParams, feedback=true, formData2) { 
+    // notification enabled by default, just turn log to false for those what requires no notification   
+    let log = true
+    if (urlParams.toString().toUpperCase().includes("QUERI")) {
+      log = false
+    }
+    log = true
+    const formData = new FormData();
+    formData.append('dbUserName', formData2[0]);
+    formData.append('dbUserPassword', formData2[1]);    
+    console.log('fetchApi, log', log, 'urlParams', urlParams, urlParams.toString().toUpperCase())
+    urlParams += "&isForTesting="+ this.config.isForTesting
+    this.dispatchEvent(new CustomEvent('set-activity', {bubbles: true, composed: true}))
+    return fetch(urlParams, {
+      method: 'POST',
+      body: formData
+    }).then(async r => {
+      if (r.status == 200) {
+        return r.json()
+      } else {
+        let err = await r.json()
+        throw err
+      }
+    }).then(j => {
       if (feedback) {
         this.dispatchEvent(new CustomEvent('success', {
           detail: {...j, log: log},
