8b8e4e1e64a7adf17074336a8a7e14159f7f4838
devofficer <myeyesontech@gmail.com>
2023-12-13 01:00:35 +0100

    FE#257 fixed implemented theming for the dashboard

--- tr-procedures/src/module_env_monit/home-water.js
+++ tr-procedures/src/module_env_monit/home-water.js
@@ -102,16 +102,32 @@ export class HomeWater extends navigator(LitElement) {
         cursor: pointer;
       }
 
-      .sample:hover {
-        stroke: #7ebfa8D0;
+      .container.primary circle,
+      .container.primary rect {
+        fill: #18a4fe;
+        stroke: #18a4fea0;
       }
 
-      .testing:hover {
-        stroke: #F4A9AAD0;
+      .container.primary circle {
+        stroke-width: 10;
       }
 
-      .results:hover {
-        stroke: #C1DBA8D0;
+      .container.primary circle:hover {
+        fill: #18a4fed0;
+      }
+
+      .container.primary path {
+        stroke: #18a4fe;
+        stroke-width: 10;
+      }
+
+      .container.primary text {
+        font-size: 20px;
+        font-weight: bold;
+        fill: white;
+        font-family: Montserrat;
+        text-anchor: middle;
+        alignment-baseline: middle;
       }
     `;
   }
@@ -166,295 +182,70 @@ export class HomeWater extends navigator(LitElement) {
     //this.resetView()
     return html`
       <svg
-        class="container"
-        viewBox="0 0 1000 600"
+        class="container primary"
+        viewBox="0 0 1200 600"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <defs>
-          <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" style="stop-color:#058a56;stop-opacity:1" />
-            <stop offset="100%" style="stop-color:#93C061;stop-opacity:1" />
-          </linearGradient>
-          <linearGradient id="grad2" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" style="stop-color:#058a56;stop-opacity:1" />
-            <stop offset="100%" style="stop-color:#93C061;stop-opacity:1" />
-          </linearGradient>
-          <linearGradient id="grad3" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" style="stop-color:#F45C5C;stop-opacity:1" />
-            <stop offset="100%" style="stop-color:#058a56;stop-opacity:1" />
-          </linearGradient>
-        </defs>
+        <path d="M 170 300 q 15 -60 60 -100" fill="none" />
+        <path d="M 170 300 q 15 60 60 100" fill="none" />
+        <rect x="770" y="195" width="60" height="10" />
+        <rect x="770" y="395" width="60" height="10" />
+        <path d="M 970 200 q 15 -30 60 100" fill="none" />
+        <path d="M 970 400 q 15 30 60 -100" fill="none" />
+        <path d="M 370 200 q 15 -30 60 100" fill="none" />
+        <path d="M 370 400 q 15 30 60 -100" fill="none" />
+        <path d="M 570 300 q 15 -60 60 -100" fill="none" />
+        <path d="M 570 300 q 15 60 60 100" fill="none" />
 
-        <path
-          d="M 170 300 q 15 -60 60 -100"
-          stroke="url(#grad1)"
-          stroke-width="10"
-          fill="none"
-        />
-        <path
-          d="M 170 300 q 15 60 60 100"
-          stroke="url(#grad1)"
-          stroke-width="10"
-          fill="none"
-        />
-        <rect x="360" y="195" width="60" height="10" fill="url(#grad2)" />
-        <rect x="570" y="195" width="60" height="10" fill="#F45C5C" />
-        <rect x="360" y="395" width="60" height="10" fill="url(#grad2)" />
-        <rect x="570" y="395" width="60" height="10" fill="#F45C5C" />
-        <path
-          d="M 770 200 q 15 -30 60 100"
-          stroke="url(#grad3)"
-          stroke-width="10"
-          fill="none"
-        />
-        <path
-          d="M 770 400 q 15 30 60 -100"
-          stroke="url(#grad3)"
-          stroke-width="10"
-          fill="none"
-        />
+        <circle cx="30" cy="300" r="20" />
+        <circle cx="170" cy="300" r="20" />
+        <circle class="sample" cx="100" cy="300" r="70" @click=${() => this.elementClicked("LogSamples", "SampleLogin")} />
+        <text x="100" y="290" @click=${() => this.elementClicked("LogSamples", "SampleLogin")}>Program</text>
+        <text x="100" y="310" @click=${() => this.elementClicked("LogSamples", "SampleLogin")}>Definition</text>
 
-        <path
-          d="M 370 200 H 60"
-          stroke="url(#grad1)"
-          stroke-width="4"
-          fill="none"
-        />
-        <circle cx="30" cy="300" r="20" fill="#058a56" />
-        <circle cx="170" cy="300" r="20" fill="#058a56" />
-        <circle
-          class="sample"
-          @click=${() => this.elementClicked("LogSamples", "SampleLogin")}
-          cx="100"
-          cy="300"
-          r="70"
-          fill="#058a56"
-          stroke="#7ebfa8a0"
-          stroke-width="10"
-        />
-        <text
-          x="100"
-          y="300"
-          @click=${() => this.elementClicked("LogSamples", "SampleLogin")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          NEW SAMPLES
-        </text>
+        <circle cx="430" cy="300" r="20" />
+        <circle cx="570" cy="300" r="20" />
+        <circle class="sample" cx="500" cy="300" r="70" />
+        <text x="500" y="300">Incubate</text>
 
-        <circle cx="230" cy="200" r="20" fill="#93C061" />
-        <circle cx="370" cy="200" r="20" fill="#93C061" />
-        <circle
-          class="results"
-          @click=${() =>
-              this.elementClicked("SampleEnterResult", "FQ Testing")}
-          cx="300"
-          cy="200"
-          r="70"
-          fill="#93C061"
-          stroke="#C1DBA8a0"
-          stroke-width="10"
-        />
-        <text
-          x="300"
-          y="200"
-          @click=${() =>
-              this.elementClicked("SampleEnterResult", "FQ Testing")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          ENTER RESULTS
-        </text>
+        <circle cx="230" cy="200" r="20" />
+        <circle cx="370" cy="200" r="20" />
+        <circle class="results" cx="300" cy="200" r="70" @click=${() => this.elementClicked("ReviewTesting", "FQ Testing")}/>
+        <text x="300" y="200" @click=${() => this.elementClicked("ReviewTesting", "FQ Testing")} >Sampling</text>
 
-        <circle cx="430" cy="200" r="20" fill="#F45C5C" />
-        <circle cx="570" cy="200" r="20" fill="#F45C5C" />
-        <circle
-          class="testing"
-          @click=${() => this.elementClicked("ReviewTesting", "FQ Testing")}
-          cx="500"
-          cy="200"
-          r="70"
-          fill="#F45C5C"
-          stroke="#F4A9AAa0"
-          stroke-width="10"
-        />
-        <text
-          @click=${() => this.elementClicked("ReviewTesting", "FQ Testing")}
-          x="500"
-          y="200"
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          REVIEW TESTS
-        </text>
+        <circle cx="630" cy="200" r="20" />
+        <circle cx="770" cy="200" r="20" />
+        <circle class="testing" cx="700" cy="200" r="70" @click=${() => this.elementClicked("ReviewTesting", "FQ Testing")} />
+        <text x="700" y="190" @click=${() => this.elementClicked("ReviewTesting", "FQ Testing")}>Plate</text>
+        <text x="700" y="210" @click=${() => this.elementClicked("ReviewTesting", "FQ Testing")}>Reading</text>
 
-        <circle cx="630" cy="200" r="20" fill="#F45C5C" />
-        <circle cx="770" cy="200" r="20" fill="#F45C5C" />
-        <circle
-          class="testing"
-          @click=${() =>
-              this.elementClicked("ReviewTestingGroup", "FQ Testing")}
-          cx="700"
-          cy="200"
-          r="70"
-          fill="#F45C5C"
-          stroke="#F4A9AAa0"
-          stroke-width="10"
-        />
-        <text
-          x="700"
-          y="190"
-          @click=${() =>
-              this.elementClicked("ReviewTestingGroup", "FQ Testing")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          REVIEW
-        </text>
-        <text
-          x="700"
-          y="210"
-          @click=${() =>
-              this.elementClicked("ReviewTestingGroup", "FQ Testing")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          TESTING GROUP
-        </text>
+        <circle cx="830" cy="200" r="20" />
+        <circle cx="970" cy="200" r="20" />
+        <circle class="testing" cx="900" cy="200" r="70" @click=${() => this.elementClicked("ReviewTestingGroup", "FQ Testing")}/>
+        <text x="900" y="190" @click=${() => this.elementClicked("ReviewTestingGroup", "FQ Testing")}>Identification of</text>
+        <text x="900" y="210" @click=${() => this.elementClicked("ReviewTestingGroup", "FQ Testing")}>microorganism</text>
 
-        <circle cx="230" cy="400" r="20" fill="#93C061" />
-        <circle cx="370" cy="400" r="20" fill="#93C061" />
-        <circle
-          class="results"
-          @click=${() =>
-              this.elementClicked("SampleEnterResult", "MB Testing")}
-          cx="300"
-          cy="400"
-          r="70"
-          fill="#93C061"
-          stroke="#C1DBA8a0"
-          stroke-width="10"
-        />
-        <text
-          x="300"
-          y="400"
-          @click=${() =>
-              this.elementClicked("SampleEnterResult", "MB Testing")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          ENTER RESULTS
-        </text>
+        <circle cx="230" cy="400" r="20" />
+        <circle cx="370" cy="400" r="20" />
+        <circle class="results" cx="300" cy="400" r="70" @click=${() => this.elementClicked("SampleEnterResult", "MB Testing")}/>
+        <text x="300" y="400" @click=${() => this.elementClicked("SampleEnterResult", "MB Testing")}>Sampling</text>
 
-        <circle cx="430" cy="400" r="20" fill="#F45C5C" />
-        <circle cx="570" cy="400" r="20" fill="#F45C5C" />
-        <circle
-          class="testing"
-          @click=${() =>
-              this.elementClicked("ReviewTesting", "MB Testing")}
-          cx="500"
-          cy="400"
-          r="70"
-          fill="#F45C5C"
-          stroke="#F4A9AAa0"
-          stroke-width="10"
-        />
-        <text
-          x="500"
-          y="400"
-          @click=${() =>
-              this.elementClicked("ReviewTesting", "MB Testing")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          REVIEW TESTS
-        </text>
+        <circle cx="630" cy="400" r="20" />
+        <circle cx="770" cy="400" r="20" />
+        <circle class="testing" cx="700" cy="400" r="70" @click=${() => this.elementClicked("ReviewTesting", "MB Testing")} />
+        <text x="700" y="390" @click=${() => this.elementClicked("ReviewTesting", "MB Testing")}>Plate</text>
+        <text x="700" y="410" @click=${() => this.elementClicked("ReviewTesting", "MB Testing")}>Reading</text>
 
-        <circle cx="630" cy="400" r="20" fill="#F45C5C" />
-        <circle cx="770" cy="400" r="20" fill="#F45C5C" />
-        <circle
-          class="testing"
-          @click=${() =>
-              this.elementClicked("ReviewTestingGroup", "MB Testing")}
-          cx="700"
-          cy="400"
-          r="70"
-          fill="#F45C5C"
-          stroke="#F4A9AAa0"
-          stroke-width="10"
-        />
-        <text
-          x="700"
-          y="390"
-          @click=${() =>
-              this.elementClicked("ReviewTestingGroup", "MB Testing")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          REVIEW
-        </text>
-        <text
-          x="700"
-          y="410"
-          @click=${() =>
-              this.elementClicked("ReviewTestingGroup", "MB Testing")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          TESTING GROUP
-        </text>
+        <circle cx="830" cy="400" r="20" />
+        <circle cx="970" cy="400" r="20" />
+        <circle class="testing" cx="900" cy="400" r="70" @click=${() => this.elementClicked("ReviewTestingGroup", "MB Testing")} />
+        <text x="900" y="390" @click=${() => this.elementClicked("ReviewTestingGroup", "MB Testing")}>Identification of</text>
+        <text x="900" y="410" @click=${() => this.elementClicked("ReviewTestingGroup", "MB Testing")}>microorganism</text>
 
-        <circle cx="830" cy="300" r="20" fill="#058a56" />
-        <circle cx="970" cy="300" r="20" fill="#058a56" />
-        <circle
-          class="sample"
-          @click=${() => this.elementClicked("ReviewSample", "")}
-          cx="900"
-          cy="300"
-          r="70"
-          fill="#058a56"
-          stroke="#7ebfa8a0"
-          stroke-width="10"
-        />
-        <text
-          x="900"
-          y="300"
-          @click=${() => this.elementClicked("ReviewSample", "")}
-          font-size="16"
-          font-weight="600"
-          fill="white"
-          text-anchor="middle"
-          alignment-baseline="middle"
-        >
-          REVIEW SAMPLE
-        </text>
+        <circle cx="1030" cy="300" r="20" />
+        <circle cx="1170" cy="300" r="20" />
+        <circle class="sample" cx="1100" cy="300" r="70" @click=${() => this.elementClicked("ReviewSample", "")} />
+        <text x="1100" y="300" @click=${() => this.elementClicked("ReviewSample", "")}>Inquires</text>
       </svg>
       <!-- <div class="maindiv">
         <div
@@ -468,7 +259,7 @@ export class HomeWater extends navigator(LitElement) {
         <div class="block-background" style="left:0px;">
           <div
             @click=${() =>
-              this.elementClicked("SampleEnterResult", "FQ Testing")}
+        this.elementClicked("SampleEnterResult", "FQ Testing")}
             class="node"
             style="top:-6px; padding: 15px 20px;"
           >
@@ -483,7 +274,7 @@ export class HomeWater extends navigator(LitElement) {
           </div>
           <div
             @click=${() =>
-              this.elementClicked("ReviewTestingGroup", "FQ Testing")}
+        this.elementClicked("ReviewTestingGroup", "FQ Testing")}
             class="node"
             style="left: 10px; top:-6px;padding: 15px 20px;"
           >
@@ -493,15 +284,14 @@ export class HomeWater extends navigator(LitElement) {
         <div class="block-background" style="top: -8px; left:140px;">
           <div
             @click=${() =>
-              this.elementClicked("SampleEnterResult", "MB Testing")}
+        this.elementClicked("SampleEnterResult", "MB Testing")}
             class="node"
             style="top:-6px; padding: 15px 20px;"
           >
             <span>Enter <br />Results</span>
           </div>
           <div
-          @click=${() =>
-              this.elementClicked("SampleEnterResult", "MB Testing")}
+          @click=${() => this.elementClicked("SampleEnterResult", "MB Testing")}
             class="node"
             style="left: 10px; top:-5px; padding: 15px 20px;"
           >
@@ -509,7 +299,7 @@ export class HomeWater extends navigator(LitElement) {
           </div>
           <div
             @click=${() =>
-              this.elementClicked("ReviewTestingGroup", "MB Testing")}
+        this.elementClicked("ReviewTestingGroup", "MB Testing")}
             class="node"
             style="left: 10px; top:-5px; padding: 15px 20px;"
           >
