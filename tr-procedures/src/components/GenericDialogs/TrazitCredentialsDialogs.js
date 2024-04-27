62d46a5b25f3a0798bdc196a6e68b3ce5e738125
frangomezvenegas <info.fran.gomez@gmail.com>
2024-04-27 22:51:28 +0200

    removed credsCheckerCommons, the next step will be on remove nextRequestCommons but in use by Programs component

--- tr-procedures/src/components/GenericDialogs/TrazitCredentialsDialogs.js
+++ tr-procedures/src/components/GenericDialogs/TrazitCredentialsDialogs.js
@@ -475,30 +475,6 @@ export function TrazitCredentialsDialogs(base) {
     return html`<p class=${this.attempt==0?'attemptsphraseblue':'attemptsphrasered'}>${txt}</p>`
   }
 
-  credsCheckerCommons(actionName, objId, params={}, action) {
-    console.log('credsCheckerCommons', 'actionName', actionName, 'action', action)
-    this.actionObj = action || {}
-    this.reqParams = params
-    if (actionName) {
-      this.actionName = actionName
-      if (objId!==undefined&&objId == -1) {
-        this.credDialog.show()
-      } else {
-        this.objectId = objId
-        let noNeedCreds = this.checkProcList()
-        if (noNeedCreds) {
-          this.nextRequestCommons(action)
-        } else {
-          if (this.type == "confirm") {
-            this.confirmDialog.show()
-          } else {
-            this.credDialog.show()
-          }
-        }
-      }
-    }
-  }
-
   nextRequestCommons(action) {
     console.log('nextRequestCommons')
     this.reqParams = {
