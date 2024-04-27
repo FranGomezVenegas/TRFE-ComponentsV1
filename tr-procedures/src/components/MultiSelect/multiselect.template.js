5c7c9b522b899c6d7b0b68a5bb39745d11ad608e
frangomezvenegas <info.fran.gomez@gmail.com>
2024-04-27 22:23:04 +0200

    searchOptions variable has no sense, replaced by options directly

--- tr-procedures/src/components/MultiSelect/multiselect.template.js
+++ tr-procedures/src/components/MultiSelect/multiselect.template.js
@@ -12,22 +12,22 @@ export const template = (props, label) => {
     if (!Array.isArray(props.activeOptions)) {
         props.activeOptions = [];
     }
-    if (props.searchOptions===undefined){ 
-        props.searchOptions=[]
-    } else if (typeof props.searchOptions === 'string') {
+    if (props.options===undefined){ 
+        props.options=[]
+    } else if (typeof props.options === 'string') {
         // Split the string into an array using the pipe '|' as a delimiter
-        props.searchOptions = props.searchOptions.split('|');
+        props.options = props.options.split('|');
     }
-    if (!Array.isArray(props.searchOptions)) {
-        props.searchOptions = [];
+    if (!Array.isArray(props.options)) {
+        props.options = [];
     }
     if (props.allowAdhocEntries===undefined){ props.allowAdhocEntries=false}
-    //if (props.searchOptions===undefined){ props.searchOptions=[]}
+    //if (props.options===undefined){ props.options=[]}
     if (props.readOnly===undefined){ props.readOnly=false}
     if (props.displayLabel===undefined){ props.displayLabel=true}
     if (props.readOnly===true){props.clickedContainer= true}
     
-    const filteredOptions = props.searchOptions.filter(option => !props.activeOptions.includes(option));
+    const filteredOptions = props.options.filter(option => !props.activeOptions.includes(option));
     const uniqueFilteredOptions = [...new Set(filteredOptions)];
     return html`
     <div>
