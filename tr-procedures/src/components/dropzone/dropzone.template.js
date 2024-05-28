803b9c1632ecacea5da336a7f6ab89367a807bab
frangomezvenegas <info.fran.gomez@gmail.com>
2024-05-28 10:36:55 +0200

    change file selector to accept all files not just images

--- tr-procedures/src/components/dropzone/dropzone.template.js
+++ tr-procedures/src/components/dropzone/dropzone.template.js
@@ -4,11 +4,11 @@ export const template = (props) => {
   const { name, label, handleUpload } = props;
   return html`
     <div class="container">
-      <input type="file" id="file-selector" multiple accept="image/*" />
+      <input type="file" id="file-selector" multiple accept="*/*" />
       <label for="file-selector">
         Choose Or Drop Document
       </label>
-      <mwc-icon-button class="button" icon="upload_file" id="downloadable" 
+      <mwc-icon-button class="button" icon="upload_file" id="uploadfile" 
         title="Click to upload the file"  @click=${handleUpload}></mwc-icon-button>    
       <div id="error"></div>
       <div id="preview"></div>
